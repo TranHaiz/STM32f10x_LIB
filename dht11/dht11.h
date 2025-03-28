a5d98deb1800196e36f327e6595f47bcbe640a85
TranHaiz <tranhai002456@gmail.com>
2025-03-29 00:07:17 +0700

    first commit

---
+++ dht11/dht11.h
@@ -0,0 +1,11 @@
+#ifndef DHT11_H_
+#define DHT11_H_
+
+#include "systick.h"
+#include "gpio.h"
+
+float dht_hum(void);
+float dht_temp(void);
+void dht_init(GPIO_TypeDef *GPIOx,uint8_t pin);
+
+#endif
